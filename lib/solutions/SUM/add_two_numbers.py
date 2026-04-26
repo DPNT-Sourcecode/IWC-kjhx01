f979552f14ac118ebd56dde9904e3782af8db6ba
sbx_user1051 <sbx_user1051@169.254.33.153>
2026-04-26 19:10:38 +0000

    Sun Apr 26 19:10:38 UTC 2026

---
+++ lib/solutions/SUM/add_two_numbers.py
@@ -0,0 +1,9 @@
+def add_two_numbers(num1: int, num2: int) -> int:
+    min_bound = 0
+    max_bound = 100
+
+    num1_in_range = min_bound <= num1 and num1 <= max_bound
+    num2_in_range = min_bound <= num2 and num2 <= max_bound
+    if num1_in_range and num2_in_range:
+        return sum([num1, num2])
+    raise ValueError(f"Inputs outisde of the range of {min_bound} - {max_bound}")
